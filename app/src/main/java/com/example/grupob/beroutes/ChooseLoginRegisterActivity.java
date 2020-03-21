882d535ae40af08646d77ad6bbe3fd116bc1fb6e
Natalia Pérez <natalia_perez_daoa1819@cev.com>
2020-03-21 20:50:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/grupob/beroutes/ChooseLoginRegisterActivity.java
@@ -0,0 +1,38 @@
+package com.example.grupob.beroutes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_register);
+
+        Button loginButton;
+        Button registerButton;
+
+        loginButton = findViewById(R.id.choose_login_button);
+        registerButton = findViewById(R.id.choose_register_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),LoginActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),RegisterActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/grupob/beroutes/RecoverPassActivity.java
@@ -0,0 +1,42 @@
+package com.example.grupob.beroutes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RecoverPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_pass);
+
+        Button recoverPassButton;
+        TextView loginButton;
+
+        recoverPassButton = findViewById(R.id.recoverPass_button);
+        loginButton = findViewById(R.id.recoverPass_returnToLogin);
+
+        recoverPassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),RecoverPassActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),LoginActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+
+    }
+}
