010cf04cf2dc65cf510d6c0bf1b1ac1b32f84f54
Natalia Pérez <natalia_perez_daoa1819@cev.com>
2020-04-28 07:29:36 +0200

    Sprint IV
    Solución de algunos errores, limpieza de código y creación de la pantalla de detalle de rutas.

---
+++ app/src/main/java/com/example/grupob/beroutes/DetailsRouteActivity.java
@@ -0,0 +1,14 @@
+package com.example.grupob.beroutes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_route);
+    }
+}
--- app/src/main/java/com/example/grupob/beroutes/RoutesListActivity.java
+++ app/src/main/java/com/example/grupob/beroutes/RoutesListActivity.java
@@ -1,7 +1,14 @@
 package com.example.grupob.beroutes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RoutesListActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class RoutesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes_list);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_favorites, R.id.navigation_search, R.id.navigation_create)
+                .build();
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
     }
 }
