cca5be6f77b00c280d5f4934082be1f9907c0c87
Yassine <mohamed_yassine_daoa1819@cev.com>
2020-05-10 18:19:38 +0200

    correccion errores/ añadir pantallas que faltaban
    Se han añadido clases,layouts y recursos que han desaparecido. Tambien se han corregido errores.

--- app/src/main/java/com/example/grupob/beroutes/Account/AccountSettingActivity.java
+++ app/src/main/java/com/example/grupob/beroutes/Account/AccountSettingActivity.java
@@ -3,17 +3,21 @@ package com.example.grupob.beroutes.Account;
 
 import android.content.Context;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-
+import android.widget.RelativeLayout;
+import com.example.grupob.beroutes.Profile.EditProfileFragment;
+import com.example.grupob.beroutes.Profile.SignOutFragment;
 import com.example.grupob.beroutes.R;
-
+import com.example.grupob.beroutes.Utils.SectionsStatePagerAdapter;
 import java.util.ArrayList;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class AccountSettingActivity extends AppCompatActivity {
 
@@ -21,12 +25,18 @@ public class AccountSettingActivity extends AppCompatActivity {
 
     private Context mContext;
 
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
         mContext = AccountSettingActivity.this;
         Log.d(TAG, "onCreate: started.");
+        mViewPager = findViewById(R.id.container);
+        mRelativeLayout = findViewById(R.id.relLayout1);
 
         setupSettingsList();
 
@@ -42,15 +52,37 @@ public class AccountSettingActivity extends AppCompatActivity {
 
     }
 
+    private void setupFragment(){
+
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new EditProfileFragment(), getString(R.string.edit_Profile_fragment)); //fragment 0
+        pagerAdapter.addFragment(new SignOutFragment(), getString(R.string.sign_out_fragment)); //fragment 1
+    }
+
+    private void setViewPager(int fragmentNumber){
+        mRelativeLayout.setVisibility((View.GONE));
+        Log.d(TAG, "setViewPager: navigation to fragment #: " + fragmentNumber);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     private void setupSettingsList(){
         Log.d(TAG, "setupSettingsList: initializing 'Account Settings' list ");
         ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
 
         ArrayList<String> option = new ArrayList<>();
-        option.add(getString(R.string.edit_Profile));
-        option.add(getString(R.string.sign_out));
+        option.add(getString(R.string.edit_Profile_fragment)); //fragment 0
+        option.add(getString(R.string.sign_out_fragment)); //fragment 1
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(mContext, android.R.layout.simple_list_item_1, option);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: navigating to fragment#: " + position);
+                setViewPager(position);
+            }
+        });
     }
 }
